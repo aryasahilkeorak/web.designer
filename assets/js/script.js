018bbde71f86100f97bd3994fc939123f0335e7a
aryasahilkeorak <sahilkashyap30693@gmail.com>
2025-03-17 12:32:41 +0530

    navbar related issues fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
     const navbarCollapse = document.querySelector(".pp-navbar-collapse");
 
     if (toggler && navbarCollapse) {
+        // Toggle the navbar on click of the toggler
         toggler.addEventListener("click", function () {
             navbarCollapse.classList.toggle("show");
             toggler.classList.toggle("collapsed");
+        });
+
+        // Close the navbar when any nav link is clicked
+        document.querySelectorAll(".pp-nav-link").forEach(function (navLink) {
+            navLink.addEventListener("click", function () {
+                // Check if the navbar is currently open
+                if (navbarCollapse.classList.contains("show")) {
+                    navbarCollapse.classList.remove("show");
+                    toggler.classList.remove("collapsed");
+                }
+            });
         });
     }
 });
